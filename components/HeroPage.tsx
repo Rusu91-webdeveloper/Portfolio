f45fa8f9aff9a23d6257fe71e9d15c32f822289c
Emanuel <emanuel-marius.rusu@dci-student.orgEmanuelRusuitist199!>
2024-06-06 15:13:53 +0200

    4th commit

--- components/HeroPage.tsx
+++ components/HeroPage.tsx
@@ -39,14 +39,14 @@ const HeroPage = () => {
 
     const timer = setTimeout(handleTyping, typingSpeed);
     return () => clearTimeout(timer);
-  }, []);
+  }, [displayedText, isDeleting, loopNum, typingSpeed, texts]);
 
   return (
     <div className="flex flex-col max-h[100vh] min-h-screen huge:py-20">
       <div className="pt-28 mdLg:flex-row mdLg:flex     mdLg:justify-between mdLg:px-4   mdLg:mb-[1rem]   flex w-full flex-col items-center p-4 ">
         <div className=" mdLg:w-[60%] mdLg:text-left w-[100%] text-center tracking-widest text-slate-300">
           <h1 className="mdLg:text-4xl mb-6 inline-block bg-gradient-to-r from-pink-800 to-violet-900 bg-clip-text text-3xl  font-bold text-transparent lg:text-6xl ">
-            Hello,I`&apos`m Emanuel
+            Hello,I&apos;m Emanuel
           </h1>
 
           <h2 className=" mdLg:text-3xl mb-4 font-mono text-2xl">
@@ -55,7 +55,7 @@ const HeroPage = () => {
           </h2>
           <p className="mb-4 text-[#b6b6b6] ">
             Welcome to my digital playground, where lines of code transform into
-            immersive online experiences. I`&apos`m Allen, a passionate and
+            immersive online experiences. I&apos;m Emanuel, a passionate and
             innovative web developer dedicated to crafting elegant and efficient
             solutions.
           </p>
