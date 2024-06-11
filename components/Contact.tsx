363a80a35e1077452af30e6c6ef4385236cf032d
Emanuel <emanuel-marius.rusu@dci-student.orgEmanuelRusuitist199!>
2024-06-11 09:51:32 +0200

    change in contact component

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -26,10 +26,10 @@ const Contact = () => {
       <div className="flex flex-col md:grid md:grid-cols-2 md:gap-4 gap-8 mt-16 lg:mt-60">
         <div>
           <h3 className="font-bold mb-8 text-gray-100 tracking-wider text-xl">
-            LET`&apos`S CONNECT
+            LET&apos;S CONNECT
           </h3>
           <p className="text-gray-300 tracking-tight">
-            I`&apos`m currently looking for new opportunities.Feel free to reach
+            I&apos;m currently looking for new opportunities.Feel free to reach
             by email or by using the contact form
           </p>
           <div
