d828368f09bd25ae66c3d59ab9a13db2aba09588
Emanuel <rusu.emanuel.webdeveloper@gmail.com>
2024-08-10 14:01:59 +0200

    .env added

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -3,40 +3,44 @@ import nodemailer from "nodemailer";
 import Mail from "nodemailer/lib/mailer";
 
 export async function POST(request: NextRequest) {
-  const { email, name, message } = await request.json();
+  try {
+    // Parse and validate request body
+    const { email, name, message } = await request.json();
+    if (!email || !name || !message) {
+      return NextResponse.json(
+        { error: "Missing required fields" },
+        { status: 400 }
+      );
+    }
 
-  const transport = nodemailer.createTransport({
-    service: "gmail",
-
-    auth: {
-      user: process.env.MY_EMAIL,
-      pass: process.env.MY_PASSWORD,
-    },
-  });
-
-  const mailOptions: Mail.Options = {
-    from: process.env.MY_EMAIL,
-    to: process.env.MY_EMAIL,
-
-    subject: `Message from ${name} (${email})`,
-    text: message,
-  };
-
-  const sendMailPromise = () =>
-    new Promise<string>((resolve, reject) => {
-      transport.sendMail(mailOptions, function (err) {
-        if (!err) {
-          resolve("Email sent");
-        } else {
-          reject(err.message);
-        }
-      });
+    // Create Nodemailer transport
+    const transport = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.MY_EMAIL,
+        pass: process.env.MY_PASSWORD,
+      },
     });
 
-  try {
-    await sendMailPromise();
+    // Define mail options
+    const mailOptions: Mail.Options = {
+      from: process.env.MY_EMAIL,
+      to: process.env.MY_EMAIL,
+      subject: `Message from ${name} (${email})`,
+      text: message,
+    };
+
+    // Send mail
+    await transport.sendMail(mailOptions);
     return NextResponse.json({ message: "Email sent" });
-  } catch (err) {
-    return NextResponse.json({ error: err }, { status: 500 });
+  } catch (err: any) {
+    // Log the error for debugging
+    console.error("Email sending failed:", err);
+
+    // Return a generic error response
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
   }
 }
